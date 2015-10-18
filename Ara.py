4871bbf788f9c370b33d57243311970dede346ca
AraHaan <seandhunt_7@yahoo.com>
2015-10-18 12:09:24 -0500

    Removed some things that do not work.

--- Ara.py
+++ Ara.py
@@ -3,7 +3,7 @@ import discord
 import logging
 import datetime
 import messageparser
-import updatecheck
+
 
 discord_user_email = 'email'
 discord_user_password = 'password'
@@ -20,31 +20,6 @@ def on_message(message):
         client.send_message(message.channel, response)
         if(message.content.startswith('!market')):
             client.send_message(message.channel,'Sorry, This Command is not ready yet.')
-    elif(message.author.id.startswith('creator_id')):
-        print('is owner')
-        if(message.content.startswith('!join')):
-            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print('joined server on ' + stamp)
-            client.accept_invite(message.content[5:].strip())
-            client.send_message(message.channel, 'Alright.')
-        elif(message.content.startswith('!leave')):
-            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            server = message.channel.server
-            print('leaving server on ' + stamp)
-            client.send_message(message.channel, 'See ya.')
-            client.leave_server(server)
-        elif(message.content.startswith('!register')):
-            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print('registering channel ' + message.channel.id + ' ' + stamp)
-            client.send_message(message.channel, updatecheck.register(message.channel.id))
-        elif(message.content.startswith('!unregister')):
-            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print('unregistering channel ' + message.channel.id + ' ' + stamp)
-            client.send_message(message.channel, updatecheck.unregister(message.channel.id))
-        elif(message.content.startswith('!list')):
-            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            print('channel list ' + ' ' + stamp)
-            client.send_message(message.channel, 'Channels registered for updates: ' + updatecheck.listeners())
 
 @client.event
 def on_ready():
