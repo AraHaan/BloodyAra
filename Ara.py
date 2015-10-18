ab9a0904a6f1c5372e1f51757c5535b5541060cc
AraHaan <seandhunt_7@yahoo.com>
2015-10-18 13:36:00 -0500

    Changed a few other things.

--- Ara.py
+++ Ara.py
@@ -14,8 +14,10 @@ client.login(discord_user_email, discord_user_password)
 @client.event
 def on_message(message):
     if(message.content.startswith('!market')):
+        print('Command not finnished.')
         client.send_message(message.channel,'Sorry, This Command is not ready yet.')
     if message.channel.server and message.channel.server.id == server_id:
+		print('Ignoreing commanads to this server' + server_id)
         return
     response = messageparser.parse(message)
     if(response is not None):
