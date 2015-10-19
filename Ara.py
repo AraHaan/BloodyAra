e3d5bdcf62670b1dfbf10e88c46164ba2841cd62
AraHaan <seandhunt_7@yahoo.com>
2015-10-19 01:17:08 -0500

    Some more shit. at least got some things working. l0l

--- Ara.py
+++ Ara.py
@@ -7,7 +7,7 @@ import messageparser
 
 discord_user_email = 'email'
 discord_user_password = 'password'
-server_id = 'SERVER_ID'
+discord_user_id = 'user_id'
 client = discord.Client()
 client.login(discord_user_email, discord_user_password)
 
@@ -16,9 +16,21 @@ def on_message(message):
     if(message.content.startswith('!market')):
         print('Command not finnished.')
         client.send_message(message.channel,'Sorry, This Command is not working properly yet.')
-    if message.channel.server and message.channel.server.id == server_id:
-        print('Ignoreing commanads to this server' + server_id)
-        return
+    elif(message.author.id.startswith(discord_user_id)):
+        print('is owner')
+        """
+        if(message.content.startswith('!join')):
+            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print('joined server on ' + stamp)
+            client.accept_invite(message.content[5:].strip("0SBTUU1wZTYiaHxf"))
+            client.send_message(message.channel, 'Alright.')
+        elif(message.content.startswith('!leave')):
+            stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            server = message.channel.server
+            print('leaving server on ' + stamp)
+            client.send_message(message.channel, 'See ya.')
+            client.leave_server(server)
+        """
     response = messageparser.parse(message)
     if(response is not None):
         client.send_message(message.channel, response)
